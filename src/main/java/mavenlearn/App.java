87ae51b687e14afd10d6f9821d7391512a0277b7
Girish Mudigonda <mudigondagirish97@gmail.com>
2024-08-14 12:51:48 +0530

    ganesh App changes

--- src/main/java/mavenlearn/App.java
+++ src/main/java/mavenlearn/App.java
@@ -3,7 +3,7 @@ package mavenlearn;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub added by ganesh
 		System.out.println("Java learning");
 
 	}
